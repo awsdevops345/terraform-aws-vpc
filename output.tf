e97a11344271ee9dc593e2a7add528d4c28160d1
awsdevops345 <naveena838693@gmail.com>
2024-01-17 22:28:04 +0530

    second commit

--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "azs" {
     value = local.aznames
 }
+
+output "vpc_id" {
+    value = aws_vpc.id
+}
+
+output "public_subnet_id" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_id" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_id" {
+    value = aws_subnet.database[*].id
+}
